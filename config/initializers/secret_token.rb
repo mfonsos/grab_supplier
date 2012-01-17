9b9b4cf6f072ae7ffd6ca3738f6cd507237d59af
mfonsos <mfonsos@gmail.com>
2012-01-17 16:52:12 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrabSupplier::Application.config.secret_token = 'e1021bd1a07336e4209773ec8631c07f48e2d58d0b35fee1d571e81061e832d30c72f55ed47bddf61ab9be5e31dea9df87a5328b5fd25bb337df073b52a232d9'
